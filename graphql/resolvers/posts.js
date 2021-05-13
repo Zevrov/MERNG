be586c5746fb8c85df19f1cb4871ef1ab3c5bcce
Wolf <zevrov.719@gmail.com>
2021-05-13 15:08:52 -0400

    added getPost to the graphql queries

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,15 +1,26 @@
 const Post = require('../../models/Post');
 
-
 module.exports = {
     Query: {
-        async getPosts(){
-            try{
+        async getPosts() {
+            try {
                 const posts = await Post.find();
                 return posts;
             } catch (err) {
                 throw new Error(err);
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found');
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
         }
     }
-}
+};
