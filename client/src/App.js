8714476079a30e48e7dbdc7bac9daca10ae9de98
Wolf <zevrov.719@gmail.com>
2021-05-14 13:18:46 -0400

    added some flavor to my text on the page and title

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <h1>Wolf Hopkins is a Great Full Stack Software Engineer</h1>
+        <h2>He would be a great addition to Softcyllic's engineering team</h2>
+      </div>
   );
 }
 
